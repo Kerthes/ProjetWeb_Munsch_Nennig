591d111a57446e78e3c5f54b69199dce728af240
nicon <nico.n.8-8@hotmail.fr>
2019-10-19 16:54:15 +0200

    ajout debut connexion

--- Connexion.php
+++ Connexion.php
@@ -1,21 +1,28 @@
-<html>
-<head>
-<title>Blog MUNSCH&NENNIG</title>
-</head>
-<body>
 <?php
 include 'Connexion_BDD.php';
- ?>
-<h1>Blog</h1>
-<h3>Connexion</h3>
-<form method="POST">
-  Identifiant <input type='text'>
-  <br>
-  <br>
-  Mot de Passe <input type='text'>
-  <br>
-  <br>
-  <input type="submit" value="Se connecter">
-</form>
-</body>
-</html>
+
+//  Récupération de l'utilisateur et de son pass hashé
+$req = $objPDO -> prepare("SELECT pseudo, motdepasse FROM nennig16u_projetweb.redacteur WHERE pseudo = :pseudo");
+
+$req->bindValue('pseudo', $_POST['pseudo']);;
+
+$resultat = $req->execute();
+
+// Comparaison du pass envoyé via le formulaire avec la base
+$isPasswordCorrect = password_verify($_POST['motdepasse'], $resultat['motdepasse']);
+
+if (!$resultat)
+{
+    echo '123Mauvais identifiant ou mot de passe !';
+}
+else
+{
+    if ($isPasswordCorrect) {
+        session_start();
+        $_SESSION['pseudo'] = $resultat['pseudo'];
+              echo 'Vous êtes connecté !';
+    }
+    else {
+        echo 'Mauvais identifiant ou mot de passe !';
+    }
+}
---
+++ PageConnexion.php
@@ -0,0 +1,21 @@
+<?php
+include 'Connexion_BDD.php';
+?>
+<html>
+<head>
+<title>Blog MUNSCH&NENNIG</title>
+</head>
+<body>
+<h1>Blog</h1>
+<h3>Connexion</h3>
+<form method="POST" action="Connexion.php">
+  Identifiant <input type='text' name="pseudo" placeholder="Entrez votre identifiant">
+  <br>
+  <br>
+  Mot de Passe <input type='text' name="motdepasse" placeholder="Entrez votre mot de passe">
+  <br>
+  <br>
+  <input type="submit" value="Se connecter">
+</form>
+</body>
+</html>
