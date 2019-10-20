7b4f9b659accaee8710b0a94b4d93a2bb855eeae
nicon <nico.n.8-8@hotmail.fr>
2019-10-20 11:26:24 +0200

    ajout ; dans connexion

--- Connexion.php
+++ Connexion.php
@@ -20,7 +20,7 @@ else
     if ($isPasswordCorrect) {
         session_start();
         $_SESSION['pseudo'] = $resultat['pseudo'];
-        $_SESSION['id']=$resultat['idredacteur']
+        $_SESSION['id']=$resultat['idredacteur'];
               echo 'Vous êtes connecté !';
     }
     else {
