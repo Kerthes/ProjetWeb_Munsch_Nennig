dfdfe65464ce94c3813c029582dffb9792f7420b
nicon <nico.n.8-8@hotmail.fr>
2019-10-20 11:04:25 +0200

    form post ajouté

---
+++ PageArticle.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+  <title>Blog MUNSCH&NENNIG </title>
+</head>
+
+<body>
+  <h3> Création d'un article <h3>
+    <p>
+      <form method="POST" action="AjoutArticle.php">
+          Titre : <input type='text' name="titre" placeholder="Entrez le titre de l'article">
+          <br>
+          <br>
+          Texte : <input type='text' name="texte" maxlength="255" size="150">
+          <br>
+          <br>
+        <p>
+          <input type="submit" value="Poster">
+      </form>
+</body>
+
+</html>
