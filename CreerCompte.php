99003a05d4192990ac357e02ce8354fb47db1067
nicon <nico.n.8-8@hotmail.fr>
2019-10-21 11:43:18 +0200

    Ajout dans connexion de verif qui affiche en alert si le contenue = null

--- CreerCompte.php
+++ CreerCompte.php
@@ -1,6 +1,64 @@
 <html>
 <head>
 <title>Blog MUNSCH&NENNIG</title>
+<script language=" javascript " type="text/javascript">
+    function verifNom(f) {
+        if (window.document.forms["formulaire"].nom.value!=""){
+            return true;
+        }
+        return false;
+    }
+
+    function verifPrenom(f) {
+        if(window.document.forms["formulaire"].prenom.value!=""){
+            return true;
+        }
+        return false;
+    }
+
+    function verifmdp(f) {
+        if (window.document.forms["formulaire"].motdepasse.value!=""){
+            return true;
+        }
+        return false;
+    }
+
+    function verifpseudo(f) {
+        if(window.document.forms["formulaire"].pseudo.value!=""){
+            return true;
+        }
+        return false;
+    }
+
+    function verifMail(f) {
+        if(window.document.forms["formulaire"].adressemail.value!=""){
+            var mail = window.document.forms["formulaire"].Mail.value;
+            var nb=0;
+            for(var i=0; i<mail.length;i++)
+            {
+                if (mail.charAt(i)=='@')
+                {
+                    nb=nb+1;
+                }
+            }
+            if(nb==1){
+                return true;
+            }
+            else
+                return false;
+        }
+        return false
+    }
+
+    function verif(f) {
+        if (verifNom(f)&&verifPrenom(f)&&verifmdp(f)&&verifpseudo(f)&&verifMail(f)){
+            return true;
+        }
+        alert("Erreur : veuillez remplir correctement les champs");
+        return false;
+    }
+</script>
+
 </head>
 <body>
 <?php
@@ -8,7 +66,7 @@ include 'Connexion_BDD.php';
 ?>
 <h1>Blog</h1>
 <h3>Création d'un compte</h3>
-<form method="POST" action="AjoutCompte.php">
+<form method="POST" name="formulaire" onsubmit="return verif();" action="AjoutCompte.php" >
   Nom <input type='text' name="nom" placeholder="Entrez votre nom">
   <br>
   <br>
