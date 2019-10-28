a095b02fb3a7e8f70f4349bfadaeb09366aa81e6
NENNIG Nicolas <51113828+Kerthes@users.noreply.github.com>
2019-10-28 18:37:09 +0100

    Update AjoutCompte.php
    commentaire ajoutcompte

--- AjoutCompte.php
+++ AjoutCompte.php
@@ -1,6 +1,7 @@
 <?php
 include 'Connexion_BDD.php';
 
+// verif si la variable est set et la donne à une autre variable locale
 if(isset($_POST['nom'])){
   $nom=$_POST['nom'];
 }
@@ -21,11 +22,15 @@ if(isset($_POST['pseudo'])){
   $pseudo=$_POST['pseudo'];
 }
 
+//les verif sont mise à true
 $verifPseudo = true;
 $verifEmail = true;
 
-//verif
+//requete verif pour recup les adresse et pseudo
 $result = $objPDO -> query("select pseudo, adressemail from redacteur");
+
+
+// verif de l'existence du pseudo / @ mail dans la bdd et mise en false de la variable si existant
 foreach ($result as $row) {
   if($_POST['pseudo']==$row['pseudo']){
     $verifPseudo = false;
@@ -49,6 +54,7 @@ if($verifPseudo==true && $verifEmail==true){
 
   header("Location:Accueil.php");
 }
+// renvoie un id d'erreur selon la variable deja existante qui va afficher l'erreur dans la page de créer un compte + redirection vers la page
 else if ($verifPseudo==true) header("Location:CreerCompte.php?erreur=1");
 else if ($verifEmail==true) header("Location:CreerCompte.php?erreur=2");
 else header("Location:CreerCompte.php?erreur=3");
