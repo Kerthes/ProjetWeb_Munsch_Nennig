83ee286ec8c472ef3c6cfbd3a1839c50186f2603
NENNIG Nicolas <51113828+Kerthes@users.noreply.github.com>
2019-10-28 12:47:43 +0100

    Update Connexion_BDD.php

--- Connexion_BDD.php
+++ Connexion_BDD.php
@@ -1,7 +1,7 @@
 <?php
 try{
   $objPDO=new PDO('mysql:host=devbdd.iutmetz.univ-lorraine.fr;
-  port=3306;dbname=nennig16u_projetweb','nennig16u_appli','31802655', array(
+  port=3306;dbname=','','', array(
     PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
   /*echo "Connexion OK";*/
 }
